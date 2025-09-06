50eaaf4e8bf8f95057b6182beb17f5ce268d156b
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2025-09-06 09:29:32 +0100

    Inital WebGpu buffers, pipeline, command encoder Partial annotation of WebGpu interface

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,132 @@
+import { type IGpu, type IGpuCanvasContext } from "./interface";
+
+// Annotate global bitwise values
+declare var GPUBufferUsage: {
+	readonly VERTEX: number;
+	readonly COPY_DST: number;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-
 	const main = document.getElementById("main");
 	if (main == null) {
-		throw new Error("Unable to mount application to #main");
+		throw Error("Unable to mount application to #main");
 	}
 	// main.innerText = "Hello, world!";
 
 	const WIDTH = 640;
 	const HEIGHT = 480;
-
 	const canvas = document.createElement("canvas");
 	canvas.width = WIDTH;
 	canvas.height = HEIGHT;
 	main.append(canvas);
 
-	const context = canvas.getContext("2d");
-	if (context == null) {
-		throw new Error("Unable to get 2d canvas context");
+	if ("gpu" in navigator) {
+		initWebGpu(canvas, navigator.gpu as IGpu);
+	} else {
+		const context = canvas.getContext("2d");
+		if (context == null) {
+			throw Error("Unable to get 2d canvas context");
+		}
+		// Write text about half way down canvas and a quarter of the way in
+		context.font = "24px serif";
+		context.fillText("WebGPU is not supported", WIDTH >> 2, HEIGHT >> 1);
 	}
 
-	// Write text about half way down canvas and a quarter of the way in
-	context.font = "24px serif";
-	context.fillText("Hello, world!", WIDTH >> 2, HEIGHT >> 1);
-
 });
+
+async function initWebGpu(canvas: HTMLCanvasElement, gpu: IGpu) {
+	const context = canvas.getContext("webgpu") as unknown as IGpuCanvasContext;
+	if (context == null) {
+		throw Error("Unable to get webgpu canvas context");
+	}
+
+	const adapter = await gpu.requestAdapter();
+	if (adapter == null) {
+		throw Error("Request WebGPU adapter failed");
+	}
+	const device = await adapter.requestDevice();
+
+	// Prepare context for WebGPU rendering
+	context.configure({
+		device,
+		format: gpu.getPreferredCanvasFormat(),
+		alphaMode: "premultiplied"
+	});
+
+	// Prepare shaders
+	const shaderSrc = document.getElementById("shaders");
+	if (shaderSrc == null) {
+		throw Error("Unable to locate shaders #shaders");
+	}
+	const module = device.createShaderModule({
+		code: shaderSrc.textContent
+	});
+
+	// Add triangle to GPU
+
+	const vertices = new Float32Array([
+		// Top
+		// xyzw
+		0.0, 0.6, 0, 1,
+		// rgba
+		1, 0, 0, 1,
+
+		// Bottom-left
+		-0.5, -0.6, 0, 1,
+		0, 1, 0, 1,
+
+		// Bottom-right
+		0.5, -0.6, 0, 1,
+		0, 0, 1, 1,
+	]);
+
+	const vertexBuffer = device.createBuffer({
+		size: vertices.byteLength,
+		usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+	});
+
+	device.queue.writeBuffer(
+		vertexBuffer, 0,
+		vertices, 0, vertices.length
+	);
+
+	// Input assembly - describe vertex assembly and wgsl entry points
+
+	const renderPipelline = device.createRenderPipeline({
+		vertex: {
+			module,
+			entryPoint: "vertexShader",
+			buffers: [{
+				attributes: [{
+					shaderLocation: 0,
+					offset: 0,
+					format: "float32x4"
+				}, {
+					shaderLocation: 1,
+					offset: 16,
+					format: "float32x4"
+				}],
+				arrayStride: 32
+			}]
+		},
+		fragment: {
+			module,
+			entryPoint: "fragmentShader",
+			targets: [{
+				format: gpu.getPreferredCanvasFormat()
+			}]
+		},
+		// Default primitive assemble, here for illustration purposes
+		primitive: {
+			topology: "triangle-list"
+		},
+		layout: "auto"
+	});
+
+	console.debug(renderPipelline);
+
+	const commandEncoder = device.createCommandEncoder();
+
+	// https://developer.mozilla.org/en-US/docs/Web/API/WebGPU_API#running_a_rendering_pass
+	console.debug(commandEncoder);
+}
---
+++ src/interface.ts
@@ -0,0 +1,128 @@
+// WebGPU device
+
+type bytes = number;
+
+interface IPipelineBase {
+	readonly entryPoint?: string;
+	readonly constants?: Record<string, number | boolean>;
+	readonly module: IGpuShaderModule;
+}
+
+interface IVertexPipeline extends IPipelineBase {
+	readonly buffers: ReadonlyArray<{
+		readonly arrayStride: bytes;
+		readonly attributes: ReadonlyArray<{
+			readonly shaderLocation: number;
+			readonly offset: bytes;
+			// https://gpuweb.github.io/gpuweb/#enumdef-gpuvertexformat
+			// TODO uint, sint, unorm, snorm, float16, etc.
+			readonly format: "float32" | "float32x2" | "float32x3" | "float32x4";
+		}>;
+		readonly stepMode?: "vertex" | "instanced";
+	}>
+}
+
+interface IFragmentPipeline extends IPipelineBase {
+	// https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice/createRenderPipeline#fragment_object_structure
+	readonly targets: ReadonlyArray<{
+		// readonly blend
+		// https://gpuweb.github.io/gpuweb/#enumdef-gputextureformat
+		// Typically rgba 16/32 float, unorm, int, etc. See getPreferredCanvasFormat
+		readonly format: string;
+		// readonly writeMask
+	}>;
+}
+
+interface IPipelineDescriptor { }
+
+interface IPipelineDescriptorConfig {
+	readonly label?: string;
+	readonly vertex: IVertexPipeline;
+	// https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice/createRenderPipeline#depthstencil_object_structure
+	readonly depthStencil?: {
+		readonly depthBias?: number;
+		readonly depthBiasClamp?: number;
+		readonly depthBiasSlopeScale?: number;
+		readonly depthWriteEnabled?: boolean;
+		// TODO populate from MDN docs
+	};
+	// https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice/createRenderPipeline#primitive_object_structure
+	readonly primitive?: {
+		/** Cull polygons based on facing direction */
+		readonly cullMode?: "back" | "front" | "none";
+		/** Define vertex order that is condidered front-facing */
+		readonly frontFace?: "ccw" | "cw";
+		readonly stripIndexFormat?: "uint16" | "uint32";
+		readonly topology?: "line-list" | "line-strip" | "point-list" | "triangle-list" | "triangle-strip";
+		readonly unclippedDepth?: boolean;
+	};
+	readonly fragment?: IFragmentPipeline;
+	// https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice/createRenderPipeline#multisample_object_structure
+	// readonly multisample?: {};
+	readonly layout: "auto";
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/GPUShaderModule
+interface IGpuShaderModule { }
+
+interface IGpuShaderModuleOpts {
+	readonly label?: string;
+	readonly code: string;
+	readonly hints?: Record<string, string>;
+	// readonly sourceMap
+}
+
+interface IBuffer { }
+
+interface IBufferOpts {
+	readonly size: number;
+	readonly usage: unknown;
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/GPUQueue
+interface IGpuQueue {
+	writeBuffer<T>(
+		buffer: IBuffer, bufferOffset: number,
+		data: T, dataOffset: number,
+		size?: number
+	): void;
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/GPUCommandEncoder
+interface IGpuCommandEncoder { }
+
+interface IDevice {
+	createShaderModule(opts: IGpuShaderModuleOpts): IGpuShaderModule;
+	createBuffer(opts: IBufferOpts): IBuffer;
+	createRenderPipeline(descriptor: IPipelineDescriptorConfig): IPipelineDescriptor;
+	createCommandEncoder(descriptor?: { readonly label?: string }): IGpuCommandEncoder;
+	readonly queue: IGpuQueue;
+}
+
+interface IGpuAdapter {
+	requestDevice(): Promise<IDevice>;
+}
+
+type TCanvasFormat = "bgra8unorm" | "rgba8unorm" | "rgba16float";
+
+export interface IGpu {
+	requestAdapter(): Promise<IGpuAdapter>;
+	getPreferredCanvasFormat(): TCanvasFormat;
+}
+
+// WebGPU canvas context
+
+// https://developer.mozilla.org/en-US/docs/Web/API/GPUCanvasContext/configure
+interface IGpuCanvasConfig {
+	readonly device: IDevice;
+	readonly format: TCanvasFormat;
+	readonly alphaMode?: "opaque" | "premultiplied";
+	readonly colorspace?: "srgb" | "display-p3";
+	// readonly toneMapping
+	// readonly usage
+	// readonly viewFormats
+}
+
+export interface IGpuCanvasContext {
+	configure(config: IGpuCanvasConfig): void;
+}
